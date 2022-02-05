69c11d065c477594178ad42bbe1d1beef296d625
Max Herold <maxoheyn@gmail.com>
2022-02-05 13:36:43 -0500

    init commit, unity project setup

---
+++ hackmanChallenge/Assets/Scripts/APIHelper.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Net;
+using System.IO;
+
+public static class APIHelper
+{
+    public const string API_URL = "https://clemsonhackman.com/api/word?key=";
+    public const string API_KEY = "NUVGODM=";
+
+    public static string Base64Decode(string base64EncodedData)
+    {
+        var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+        return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+    }
+}
